180c136f202bf83bc2c6d5077e9a2fc158f6e43e
Roberto <betobutaku@gmail.com>
2026-08-05 13:19:22 -0600

    prueba para arreglar el microservicio

--- dprisa-backend/server.js
+++ dprisa-backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 const db = require('./database'); 
 
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json());
+app.use(express.static(__dirname));
 
 // --- CONFIGURACIÓN DE WEB-PUSH ---
 const publicVapidKey = process.env.publicVapidKey ? process.env.publicVapidKey.trim() : '';
